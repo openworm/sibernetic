0bb19403669b8d3c0b376aa93e59ea373c7c50f1
Mike Vella <vellamike@gmail.com>
2013-12-15 20:24:27 +0000

    buffers folder added and some more debugging in main.cpp

---
+++ buffers/README.md
@@ -0,0 +1,1 @@
+Results of simulation saved here.
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include "owWorldSimulation.h"
 #include <stdio.h>
+#include <iostream>
 
 extern bool load_to_file = false;
 extern bool load_from_file = false;
 int main(int argc, char **argv)
 {
 	if(argc == 1)
+	{
+		std::cout << "No arguments, run method executing\n";
 		run( argc, argv);
+	}
 	else{
 		bool graph = true;
 		for(int i = 1; i<argc; i++){
 			if(strncmp(argv[i], "-no_g", 5) == 0)//run without graphics
 				graph = false;
 			if(strncmp(argv[i], "-l_to", 5) == 0){//run load config to file mode
+				std::cout << "l_to flag, Sibernetic will save simulation results to disk\n";
 				graph = false;
 				load_to_file = true;
 			}
