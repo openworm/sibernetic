b23295b3bee3228be8d32b8a4777745265ee0f7d
Cristopher Aldama Pérez <radiomistc@gmail.com>
2014-01-02 23:59:17 -0600

    modified:   src/VectorMath.h
    Change value parameters from Vector3D member functions for reference parameters to avoid copying objects

--- Release/src/owHelper.d
+++
@@ -1,8 +0,0 @@
-src/owHelper.d: ../src/owHelper.cpp ../src/owHelper.h \
- ../src/owOpenCLConstant.h ../src/owPhysicsConstant.h
-
-../src/owHelper.h:
-
-../src/owOpenCLConstant.h:
-
-../src/owPhysicsConstant.h:
