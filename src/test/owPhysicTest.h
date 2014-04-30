bd3ac23cc32ca63d6764d446e43ad321dd1c757b
segevara <s.khayrulin@gmail.com>
2014-04-30 13:42:13 +0700

    Add test code files

---
+++ src/test/owPhysicTest.h
@@ -0,0 +1,14 @@
+/*
+ * owPhysicTest.h
+ *
+ *  Created on: Apr 30, 2014
+ *      Author: serg
+ */
+
+#ifndef OWPHYSICTEST_H_
+#define OWPHYSICTEST_H_
+
+void test_energy_conservation();
+
+
+#endif /* OWPHYSICTEST_H_ */
