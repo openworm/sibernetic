8deec26fc786ba9519728f8bac6054f7ef36351f
Sergey Khayrulin <s.khayrulin@gmail.com>
2015-10-26 15:49:50 +0600

    Add ability to change logging step in input parameters print logstep=<value>.

--- inc/owConfigProperty.h
+++ inc/owConfigProperty.h
@@ -82,6 +82,13 @@ public:
 		if(isWormConfig())
 			simulation.setup();
 	}
+	void setTimeStep(float value){
+		time_step = value;
+	}
+	void setLogStep(int value){
+		time_step = value;
+	}
+	int getLogStep(){ return logStep; }
 	std::string getSnapshotFileName() {
 		std::string fileName = "./configuration/snapshot/" + configFileName + "_";
 		std::stringstream ss;
@@ -102,7 +109,7 @@ public:
 		return fileName;
 	}
 	// Constructor
-	owConfigProrerty(int argc, char** argv):numOfElasticP(0), numOfLiquidP(0), numOfBoundaryP(0), numOfMembranes(0), MUSCLE_COUNT(100), path("./configuration/"){
+	owConfigProrerty(int argc, char** argv):numOfElasticP(0), numOfLiquidP(0), numOfBoundaryP(0), numOfMembranes(0), MUSCLE_COUNT(100), logStep(10), path("./configuration/"){
 		preferable_device_type = ALL;
 		time_step = timeStep;
 		time_limit = 0.f;
@@ -121,15 +128,24 @@ public:
 			if(s_temp.find("timestep=") == 0){
 
 				time_step = ::atof( s_temp.substr(s_temp.find('=')+1).c_str());
+				if(time_step < 0.0)
+					std::cout << "timeStep < 0 using default value" << timeStep << std::endl;
 				time_step = (time_step > 0) ? time_step : timeStep;
 				//also we shoisSimulationRun = true;uld recalculate beta if time_step is different from default value of timeStep in owPhysicsConstant
 				beta = time_step*time_step*mass*mass*2/(rho0*rho0);
 			}
 			if(s_temp.find("timelimit=") == 0){
 				time_limit = ::atof( s_temp.substr(s_temp.find('=')+1).c_str());
+				if(time_limit < 0.0)
+					throw std::runtime_error("timelimit could not be less than 0 check input parameters");
 			}
 			if(s_temp.find("LEAPFROG") != std::string::npos || s_temp.find("leapfrog") != std::string::npos){
 				integration_method = LEAPFROG;
+			}
+			if(s_temp.find("logstep=") == 0 ){
+				logStep = ::atoi(s_temp.substr(s_temp.find('=')+1).c_str());
+				if(logStep < 1)
+					throw std::runtime_error("logStep could not be less than 1 check input parameters");
 			}
 			if(s_temp == "-f"){
 				if(i + 1 < argc){
@@ -228,6 +244,7 @@ private:
 	int PARTICLE_COUNT;
 	int PARTICLE_COUNT_RoundedUp;
 	int totalNumberOfIteration;
+	int logStep;
 	float time_step;
 	float time_limit;
 	float beta;
--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@
 bool load_from_file = false;
 bool load_to = false;
 
-char version[] = "0.0.3";
+std::string version = "0.0.3";
         
 int main(int argc, char **argv) {
     if (argc == 1) {
@@ -49,53 +49,56 @@ int main(int argc, char **argv) {
         bool graph = true;
         bool run_tests = false;
 
-        char help_flag[] = "-help";
-        char help_flag2[] = "-h";
-        char help_flag3[] = "-?";
-        char help_flag4[] = "--help";
-        char no_graphics_flag[] = "-no_g";
-        char save_flag[] = "-l_to";
-        char load_flag[] = "-l_from";
-        char test_flag[] = "-test";
-        char config_file_flag[] = "-f <filename>";
-        char config_file_worm[] = "-f worm";
-        char device_type_flag[] = "device=<device_type>";
-        char time_step_flag[] = "timestep=<value>";
-        char time_limit_flag[] = "timelimit=<value>";
-        char integration_method_flag[] = "leapfrog";
+        std::string helpFlag = "-help";
+        std::string helpFlag2 = "-h";
+        std::string helpFlag3 = "-?";
+        std::string helpFlag4 = "--help";
+        std::string noGraphicsFlag = "-no_g";
+        std::string saveFlag = "-l_to";
+        std::string loadFlag = "-l_from";
+        std:;string testFlag = "-test";
+        std::string configFileFlag = "-f <filename>";
+        std::string configFileWorm = "-f worm";
+        std::string deviceTypeFlag = "device=<device_type>";
+        std::string timeStepFlag = "timestep=<value>";
+        std::string timeLimitFlag = "timelimit=<value>";
+        std::string integrationMethodFlag = "leapfrog";
+        std::string logStepFlag = "logstep=<value>";
 
         for (int i = 1; i < argc; i++) {
-            if (strncmp(argv[i], help_flag, 6) == 0 || 
-                strncmp(argv[i], help_flag2, 6) == 0|| 
-                strncmp(argv[i], help_flag3, 6) == 0|| 
-                strncmp(argv[i], help_flag4, 6) == 0) { // print usage information
+            if (helpFlag.compare(argv[i]) == 0 ||
+            	helpFlag2.compare(argv[i]) == 0||
+				helpFlag3.compare(argv[i]) == 0||
+				helpFlag4.compare(argv[i]) == 0) { // print usage information
                 std::cout << "\nSibernetic v" << version << "\n  This is a C++ implementation of the Contractile SPH (Electrofluid) algorithm applied to C. elegans locomotion\n\n";
                 std::cout << "  Usage:  ./Release/Sibernetic [OPTION]\n\n";
-                std::cout << "    " << no_graphics_flag << "                      Run without graphics\n\n";
-                std::cout << "    " << save_flag << "                      Save simulation results to disk\n\n";
-                std::cout << "    " << load_flag << "                    Load simulation results from disk\n\n";
-                std::cout << "    " << test_flag << "                      Run some tests\n\n";
-                std::cout << "    " << config_file_flag << "              Load configuration from file ./configuration/<filename>\n\n";
-                std::cout << "    " << config_file_worm << "                    **Load Worm Body Simulation**\n\n";
-                std::cout << "    " << device_type_flag << "       Trying to init OpenCL on device <type> it could be cpu or gpu default-ALL (it try to init most powerful available device)\n\n";
-                std::cout << "    " << time_step_flag << "           Start simulation with time step = <value> in seconds\n\n";
-                std::cout << "    " << time_limit_flag << "          Run simulation until <value> will be reached in seconds\n\n";
-                std::cout << "    " << integration_method_flag << "                   Run simulation using Leapfrog integration method for time integration\n\n";
-                std::cout << "    " << help_flag << "                      Print this information\n\n";
+                std::cout << "    " << noGraphicsFlag << "                      Run without graphics\n\n";
+                std::cout << "    " << saveFlag << "                      Save simulation results to disk\n\n";
+                std::cout << "        " << logStepFlag << "           Set frequency of logging data into file in -l_to mode by default it equals to 10\n\n";
+                std::cout << "    " << loadFlag << "                    Load simulation results from disk\n\n";
+                std::cout << "    " << testFlag << "                      Run some tests\n\n";
+                std::cout << "    " << configFileFlag << "              Load configuration from file ./configuration/<filename>\n\n";
+                std::cout << "        " << configFileWorm << "                    **Load Worm Body Simulation**\n\n";
+                std::cout << "    " << deviceTypeFlag << "       Trying to init OpenCL on device <type> it could be cpu or gpu default-ALL (it try to init most powerful available device)\n\n";
+                std::cout << "    " << timeStepFlag << "           Start simulation with time step = <value> in seconds\n\n";
+                std::cout << "    " << timeLimitFlag << "          Run simulation until <value> will be reached in seconds\n\n";
+                std::cout << "    " << integrationMethodFlag << "                   Run simulation using Leapfrog integration method for time integration\n\n";
+                std::cout << "    " << integrationMethodFlag << "                   Run simulation using Leapfrog integration method for time integration\n\n";
+                std::cout << "    " << helpFlag << "                      Print this information\n\n";
                 std::cout << "  Please report any bugs/issues on: https://github.com/openworm/sibernetic/issues\n\n";
                 return 0;
             }
-            if (strncmp(argv[i], no_graphics_flag, 5) == 0) // run without graphics
+            if (noGraphicsFlag.compare(argv[i]) == 0) // run without graphics
                 graph = false;
-            if (strncmp(argv[i], save_flag, 5) == 0) { // run load config to file mode
-                std::cout << save_flag << " flag: Sibernetic will save simulation results to disk\n";
+            if (saveFlag.compare(argv[i]) == 0) { // run load config to file mode
+                std::cout << saveFlag << " flag: Sibernetic will save simulation results to disk\n";
                 load_to = true;
             }
-            if (strncmp(argv[i], load_flag, 7) == 0) { // run load config from file mode
+            if (loadFlag.compare(argv[i]) == 0) { // run load config from file mode
                 graph = true;
                 load_from_file = true;
             }
-            if (strncmp(argv[i], test_flag, 5) == 0) { // run tests
+            if (testFlag.compare(argv[i]) == 0) { // run tests
                 run_tests = true;
             }
         }
