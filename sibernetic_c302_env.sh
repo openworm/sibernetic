a87bb2b5de74e1501d0ba3ad7ed9c20b8738f699
Padraig Gleeson <p.gleeson@gmail.com>
2016-11-22 19:58:06 +0000

    Removing unused file

--- sibernetic_c302_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export C302_HOME=/home/padraig/neuroConstruct/osb/invertebrate/celegans/CElegansNeuroML/CElegans/pythonScripts/c302
-
-#export PYTHONPATH=$PYTHONPATH:$C302_HOME:./src
-
