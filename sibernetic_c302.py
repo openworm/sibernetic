0a27cbcbdaa39145d31baaf2a858f2cbed5150ee
Padraig Gleeson <p.gleeson@gmail.com>
2017-11-03 17:09:14 +0000

    Allowing option to run sims with no c302, i.e. use internal python sine generators, but save simulations in same way

--- sibernetic_c302.py
+++ sibernetic_c302.py
@@ -8,7 +8,7 @@ import time
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-DEFAULTS = {'duration': 1.0,
+DEFAULTS = {'duration': 2.0,
             'dt': 0.005,
             'dtNrn': 0.05,
             'logstep': 100,
@@ -16,7 +16,8 @@ DEFAULTS = {'duration': 1.0,
             'c302params': 'C1',
             'verbose': False,
             'device': 'ALL',
-            'configuration': 'worm'} 
+            'configuration': 'worm',
+            'noc302': False} 
             
             
 def process_args():
@@ -26,6 +27,11 @@ def process_args():
     parser = argparse.ArgumentParser(
                 description=("A script which can run Sibernetic, controlled by a "+
                 "neuronal network in c302"))
+
+    parser.add_argument('-noc302', 
+                        action='store_true',
+                        default=DEFAULTS['noc302'],
+                        help="Use this flag to run Sibernetic with the inbuilt (Python based) sine wave generator, using specified duration/configuration etc. and saving in simulations directory, default: %s"%DEFAULTS['noc302'])
 
     parser.add_argument('-duration', 
                         type=float,
@@ -136,86 +142,97 @@ def run(a=None,**kwargs):
         print_("Cannot import one of the required packages. Please install!\n"
              "Exception: %s\n"%e)
     
-    try:
-        if os.environ.has_key('C302_HOME'):
-            os.environ['C302_HOME']
-            sys.path.append(os.environ['C302_HOME'])
-            print_('Python path now: %s'%sys.path)
-        import c302
-        import c302_utils
-    except Exception as e:
-        print_("Cannot import c302!\n"
-             "Exception: %s\n"%e
-             +"Please set environment variable C302_HOME to point to the directory: CElegansNeuroML/CElegans/pythonScripts/c302!\n")
-             
-        exit()
-        
     a = build_namespace(a,**kwargs)
+    
+    if not a.noc302:
+
+        try:
+            if os.environ.has_key('C302_HOME'):
+                os.environ['C302_HOME']
+                sys.path.append(os.environ['C302_HOME'])
+                print_('Python path now: %s'%sys.path)
+            import c302
+            import c302_utils
+        except Exception as e:
+            print_("Cannot import c302!\n"
+                 "Exception: %s\n"%e
+                 +"Please set environment variable C302_HOME to point to the directory: CElegansNeuroML/CElegans/pythonScripts/c302!\n")
+
+            exit()
+        
     
     gen_start = time.time()
     
-    ref = a.reference
     
     if not os.path.isdir('simulations'):
         os.mkdir('simulations')
         
     current_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
      
-    sim_ref = "%s_%s_%s" % (a.c302params, ref, current_time)
+    
+    if not a.noc302:
+
+        ref = a.reference
+        sim_ref = "%s_%s_%s" % (a.c302params, ref, current_time)
+        
+    else:
+        sim_ref = "Sibernetic_%s" % (current_time)
+        
     sim_dir = "simulations/%s" % (sim_ref)
     
     os.mkdir(sim_dir)
     
-    #exec('from %s import ParameterisedModel'%a.c302params)
-    #params = ParameterisedModel()
+    run_dir = '.'
     
-    id = '%s_%s'%(a.c302params,ref)
+    if not a.noc302:
+    
+        id = '%s_%s'%(a.c302params,ref)
     
     
-    exec('from c302_%s import setup'%ref)
+        exec('from c302_%s import setup'%ref)
     
-    setup(a.c302params, 
+        setup(a.c302params, 
           generate=True,
           duration = a.duration,
           dt = a.dt,
           target_directory=sim_dir)
     
-             
-    lems_file0 = '%s/LEMS_c302_%s.xml'%(sim_dir,id)
-    lems_file = '%s/LEMS_c302.xml'%(sim_dir)
-    print_("Renaming %s -> %s"%(lems_file0,lems_file))
-    os.rename(lems_file0,lems_file)
-    
-    announce("Generating NEURON files from: %s..."%lems_file)
-    
-    pynml.run_lems_with_jneuroml_neuron(lems_file,
-                                        only_generate_scripts=True,
-                                        nogui=True, 
-                                        load_saved_data=False, 
-                                        verbose=True)
-                                        
-    main_nrn_py = open('%s/LEMS_c302_nrn.py'%(sim_dir),'r')
-    updated =''
-    for line in main_nrn_py:
-        line = line.replace('GenericCell.hoc','%s/GenericCell.hoc'%sim_dir)
-        line = line.replace('GenericNeuronCell.hoc','%s/GenericNeuronCell.hoc'%sim_dir)
-        line = line.replace('GenericMuscleCell.hoc','%s/GenericMuscleCell.hoc'%sim_dir)
-        line = line.replace("open('time.dat","open('%s/time.dat"%sim_dir)
-        line = line.replace("open('c302_","open('%s/c302_"%sim_dir)
-        updated += line
-    main_nrn_py.close() 
-    
-    main_nrn_py = open('%s/LEMS_c302_nrn.py'%(sim_dir),'w')
-    main_nrn_py.write(updated)
-    main_nrn_py.close() 
-    
-    run_dir = '.'
-    command = 'nrnivmodl %s'%sim_dir
 
-    announce("Compiling NMODL files for NEURON...")
-    pynml.execute_command_in_dir(command, run_dir, prefix="nrnivmodl: ")
+        lems_file0 = '%s/LEMS_c302_%s.xml'%(sim_dir,id)
+        lems_file = '%s/LEMS_c302.xml'%(sim_dir)
+        print_("Renaming %s -> %s"%(lems_file0,lems_file))
+        os.rename(lems_file0,lems_file)
 
-    command = './Release/Sibernetic -c302 -f %s -no_g -l_to lpath=%s timelimit=%s timestep=%s logstep=%s device=%s'%(a.configuration, sim_dir,a.duration/1000.0,a.dt/1000,a.logstep,a.device)
+        announce("Generating NEURON files from: %s..."%lems_file)
+
+        pynml.run_lems_with_jneuroml_neuron(lems_file,
+                                            only_generate_scripts=True,
+                                            nogui=True, 
+                                            load_saved_data=False, 
+                                            verbose=True)
+
+        main_nrn_py = open('%s/LEMS_c302_nrn.py'%(sim_dir),'r')
+        updated =''
+        for line in main_nrn_py:
+            line = line.replace('GenericCell.hoc','%s/GenericCell.hoc'%sim_dir)
+            line = line.replace('GenericNeuronCell.hoc','%s/GenericNeuronCell.hoc'%sim_dir)
+            line = line.replace('GenericMuscleCell.hoc','%s/GenericMuscleCell.hoc'%sim_dir)
+            line = line.replace("open('time.dat","open('%s/time.dat"%sim_dir)
+            line = line.replace("open('c302_","open('%s/c302_"%sim_dir)
+            updated += line
+        main_nrn_py.close() 
+
+        main_nrn_py = open('%s/LEMS_c302_nrn.py'%(sim_dir),'w')
+        main_nrn_py.write(updated)
+        main_nrn_py.close() 
+
+        command = 'nrnivmodl %s'%sim_dir
+
+        announce("Compiling NMODL files for NEURON...")
+        pynml.execute_command_in_dir(command, run_dir, prefix="nrnivmodl: ")
+
+    command = './Release/Sibernetic %s -f %s -no_g -l_to lpath=%s timelimit=%s timestep=%s logstep=%s device=%s'%('' if a.noc302 else '-c302', a.configuration, sim_dir,a.duration/1000.0,a.dt/1000,a.logstep,a.device)
+        
     env={"PYTHONPATH":".:./%s"%sim_dir}
     
     sim_start = time.time()
@@ -232,8 +249,12 @@ def run(a=None,**kwargs):
     reportj['dt'] = '%s ms'%a.dt
     reportj['logstep'] = a.logstep
     reportj['sim_ref'] = sim_ref
-    reportj['reference'] = a.reference
-    reportj['c302params'] = a.c302params
+    reportj['noc302'] = a.noc302
+    reportj['python_args'] = 'python'+ ' '.join(sys.argv)
+    if not a.noc302:
+        reportj['reference'] = a.reference
+        reportj['c302params'] = a.c302params
+    
     reportj['generation_time'] = '%s s'%(sim_start-gen_start)
     reportj['run_time'] = '%s s'%(sim_end-sim_start)
     reportj['device'] = '%s'%(a.device)
@@ -245,32 +266,47 @@ def run(a=None,**kwargs):
     report_file.write(pp.pformat(reportj))
     report_file.close()
     
-    announce("Generating images for neuronal activity...")
     
-    results = pynml.reload_saved_data(lems_file, 
-                      base_dir=sim_dir,
-                      plot=False, 
-                      show_plot_already=False, 
-                      simulator=None, 
-                      verbose=True)
-                      
-    c302_utils.plot_c302_results(results,
-                                 config=a.reference, 
-                                 parameter_set=a.c302params, 
-                                 directory=sim_dir,
-                                 save=True,
-                                 show_plot_already=False)
+    if not a.noc302:
+
+        announce("Generating images for neuronal activity...")
+
+        results = pynml.reload_saved_data(lems_file, 
+                          base_dir=sim_dir,
+                          plot=False, 
+                          show_plot_already=False, 
+                          simulator=None, 
+                          verbose=True)
+
+        c302_utils.plot_c302_results(results,
+                                     config=a.reference, 
+                                     parameter_set=a.c302params, 
+                                     directory=sim_dir,
+                                     save=True,
+                                     show_plot_already=False)
                                  
     
-    pos_file_name = os.path.abspath('%s/position_buffer.txt'%sim_dir)
-    announce("Plotting positions of worm body particles in %s..."%pos_file_name)
+    ####   WCON plotting better...
+    #pos_file_name = os.path.abspath('%s/position_buffer.txt'%sim_dir)
+    #announce("Plotting positions of worm body particles in %s..."%pos_file_name)
+    #from plot_positions import plot_positions
+    #if not os.path.isfile(pos_file_name):
+    #    time.sleep(2)
+    #plot_positions(pos_file_name,rate_to_plot = int(a.duration/5), show_plot=False)
     
-    from plot_positions import plot_positions
+    from wcon.generate_wcon import generate_wcon
     
-    if not os.path.isfile(pos_file_name):
-        time.sleep(2)
-    
-    plot_positions(pos_file_name,rate_to_plot = int(a.duration/5), show_plot=False)
+    num_steps = int(a.duration/a.dt)
+    num_steps_logged = num_steps/a.logstep
+    rate = max(1,int(num_steps_logged/20.0))
+    #print("%s, %s"%(num_steps, rate))
+    generate_wcon('%s/worm_motion_log.txt'%sim_dir,
+                  '%s/worm_motion_log.wcon'%sim_dir,
+                  rate_to_plot=rate,
+                  plot=False,
+                  save_figure1_to='%s/worm_motion_1.png'%sim_dir,
+                  save_figure2_to='%s/worm_motion_2.png'%sim_dir,
+                  save_figure3_to='%s/worm_motion_3.png'%sim_dir)
     
     announce("Finished in %s sec!\n\nSimulation saved in: %s\n\n"%((sim_end-sim_start),sim_dir) + \
              "Report of simulation at: %s/report.json\n\n"%(sim_dir)+ \
--- wcon/generate_wcon.py
+++ wcon/generate_wcon.py
@@ -7,29 +7,35 @@ import numpy as np
 def dist(x1, y1, x2, y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
+def print_(msg):
+    
+    print("WCON gen >>> %s"%(msg))
 
 def generate_wcon(pos_file_name,
                   wcon_file_name,
                   rate_to_plot=1,
                   max_lines=1e12,
                   max_time_s=1e12,
-                  plot=False):
+                  plot=False,
+                  save_figure1_to=None,
+                  save_figure2_to=None,
+                  save_figure3_to=None):
+                      
+    print_("Generating WCON from %s to %s, with plotting rate %i"%(pos_file_name,wcon_file_name,rate_to_plot))
 
     postions_file = open(pos_file_name)
 
     line_num = 0
 
-    print("Loading: %s" % pos_file_name)
 
     ts = []
     x = {}
     y = {}
     z = {}
 
-    if plot:
-        import matplotlib.pyplot as plt
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
     num_plotted_frames = 0
     points_plotted = 0
@@ -52,7 +58,7 @@ def generate_wcon(pos_file_name,
 
         line_num += 1
         if line_num > max_lines:
-            print("Finished parsing file, as max number of lines reached!")
+            print_("Finished parsing file, as max number of lines reached!")
             break
 
         if line_num == 1 or line_num % rate_to_plot == 0:
@@ -64,80 +70,81 @@ def generate_wcon(pos_file_name,
             ts.append(t_s)
 
             if t_s > max_time_s:
-                print("Finished parsing file, as max time reached!")
+                print_("Finished parsing file, as max time reached!")
                 break
             x[t_s] = [float(w) for w in words[2:2 + points]]
             y[t_s] = [float(w) for w in words[3 + points:3 + 2 * points]]
             z[t_s] = [float(w) for w in words[4 + 2 * points:]]
 
-            print("L%i: at time: %s sec found %i points: [(%s,%s,%s),...,(%s,%s,%s)]" % (
+            print_("L%i: at time: %s sec found %i points: [(%s,%s,%s),...,(%s,%s,%s)]" % (
                 line_num, t_s, points, x[t_s][0], y[t_s][0], z[t_s][0], x[t_s][points-1], y[t_s][points-1], z[t_s][points-1]))
 
-            if plot:
-                spacing = 0.05
+            spacing = 0.05
 
-                xs = []
-                ys = []
-                avx = 0
-                avy = 0
-                offset = num_plotted_frames * spacing
-                for i in range(len(x[t_s])):
+            xs = []
+            ys = []
+            avx = 0
+            avy = 0
+            offset = num_plotted_frames * spacing
+            for i in range(len(x[t_s])):
 
-                    # Swap x and y so worm moves "up"
-                    xs.append(y[t_s][i] + offset)
-                    ys.append(x[t_s][i])
-                    
-                    avx += xs[-1] - offset
-                    avy += ys[-1]
-                    points_plotted += 1
-                    
-                avx = avx / points
-                avy = avy / points
+                # Swap x and y so worm moves "up"
+                xs.append(y[t_s][i] + offset)
+                ys.append(x[t_s][i])
 
-                if len(middle_points) > 0:
-                    dt = t_s - time_points[-1]
-                    middle_point_speed_x.append(
-                        (xs[middle_point] - offset -
-                         middle_points[-1][0]) / dt)
+                avx += xs[-1] - offset
+                avy += ys[-1]
+                points_plotted += 1
 
-                    middle_point_speed_y.append(
-                        (ys[middle_point] - middle_points[-1][1]) / dt)
-                    dav = dist(avx, avy, ave_points[-1][0], ave_points[-1][1])
+            avx = avx / points
+            avy = avy / points
 
-                    ave_point_speed_x.append((avx - ave_points[-1][0]) / dt)
-                    ave_point_speed_y.append((avy - ave_points[-1][1]) / dt)
+            if len(middle_points) > 0:
+                dt = t_s - time_points[-1]
+                middle_point_speed_x.append(
+                    (xs[middle_point] - offset -
+                     middle_points[-1][0]) / dt)
 
-                    print("  Speed of point" +
-                          " %i: (%s,%s) -> (%s,%s): x %sum/s, y %sum/s" %
-                          (middle_point, middle_points[-1][0],
-                           middle_points[-1][1], xs[middle_point],
-                           ys[middle_point], middle_point_speed_x[-1],
-                           middle_point_speed_y[-1]))
+                middle_point_speed_y.append(
+                    (ys[middle_point] - middle_points[-1][1]) / dt)
+                dav = dist(avx, avy, ave_points[-1][0], ave_points[-1][1])
 
-                    print("  Speed of av point " +
-                          "(%s,%s) -> : (%s,%s): x %sum/s, y %sum/s" %
-                          (ave_points[-1][0], ave_points[-1][1], avx, avy,
-                           ave_point_speed_x[-1], ave_point_speed_y[-1]))
+                ave_point_speed_x.append((avx - ave_points[-1][0]) / dt)
+                ave_point_speed_y.append((avy - ave_points[-1][1]) / dt)
 
-                middle_points.append(
-                    (xs[middle_point] - offset, ys[middle_point]))
-                ave_points.append((avx, avy))
-                time_points.append(t_s)
+                print_("  Speed of point" +
+                      " %i: (%s,%s) -> (%s,%s): x %sum/s, y %sum/s" %
+                      (middle_point, middle_points[-1][0],
+                       middle_points[-1][1], xs[middle_point],
+                       ys[middle_point], middle_point_speed_x[-1],
+                       middle_point_speed_y[-1]))
 
-                print("  Plot frame %i at %s s; l %i: [(%s,%s),...#%i]\n" % (
-                    num_plotted_frames, t_s, line_num, xs[0], ys[0], len(xs)))
-                    
-                num_plotted_frames += 1
-                
-                ax.plot(xs, ys, '-')
-                if num_plotted_frames % 5 == 1:
-                    time = '%ss' % t_s if not t_s == int(t_s) \
-                        else '%ss' % int(t_s)
-    
-                    ax.text(50 + ((num_plotted_frames - 1) * spacing),
-                            10, time, fontsize=12)
+                print_("  Speed of av point " +
+                      "(%s,%s) -> : (%s,%s): x %sum/s, y %sum/s" %
+                      (ave_points[-1][0], ave_points[-1][1], avx, avy,
+                       ave_point_speed_x[-1], ave_point_speed_y[-1]))
+
+            middle_points.append(
+                (xs[middle_point] - offset, ys[middle_point]))
+            ave_points.append((avx, avy))
+            time_points.append(t_s)
+
+            print_("  Plot frame %i at %s s; l %i: [(%s,%s),...#%i]\n" % (
+                num_plotted_frames, t_s, line_num, xs[0], ys[0], len(xs)))
+
+            num_plotted_frames += 1
+
+            ax.plot(xs, ys, '-')
+            if num_plotted_frames % 5 == 1:
+                time = '%ss' % t_s if not t_s == int(t_s) \
+                    else '%ss' % int(t_s)
+
+                ax.text(50 + ((num_plotted_frames - 1) * spacing),
+                        10, time, fontsize=12)
                             
     data = np.zeros((len(ts),len(xs)-4))
+    
+    print_('Finished parsing %i lines'%line_num)
     
     for ti in range(len(ts)):
         
@@ -161,11 +168,11 @@ def generate_wcon(pos_file_name,
             
             data[ti][i-2]= deg
             
-            print("At t=%s, i=%s: angle from between (%s,%s) - (%s,%s) - (%s,%s) = %s, %sdeg"%(ts[ti], i, x1,y1,xc,yc,x2,y2,angle,deg))
+            ###print("At t=%s, i=%s: angle from between (%s,%s) - (%s,%s) - (%s,%s) = %s, %sdeg"%(ts[ti], i, x1,y1,xc,yc,x2,y2,angle,deg))
 
     info = "Loaded: %s points from %s, saving %i frames" % (
         line_num, pos_file_name, num_frames)
-    print(info)
+    print_(info)
 
     wcon = open(wcon_file_name, 'w')
 
@@ -197,25 +204,34 @@ def generate_wcon(pos_file_name,
     wcon.close()
     postions_file.close()
 
-    if plot:
+    if save_figure1_to:
+        plt.savefig(save_figure1_to)
 
-        fig = plt.figure()
-        plt.plot(time_points[1:], middle_point_speed_x, 'cyan',
-                 label='Speed in x dir of point %i/%i' % (middle_point,
-                                                          points))
-        plt.plot(time_points[1:], middle_point_speed_y, 'red',
-                 label='Speed in y dir of point %i/%i' % (middle_point,
-                                                          points))
-        plt.plot(time_points[1:], ave_point_speed_x, 'blue',
-                 label='Speed in x of average of %i points' % points)
-        plt.plot(time_points[1:], ave_point_speed_y, 'green',
-                 label='Speed in y of average of %i points' % points)
-        plt.legend()
-        
-        fig = plt.figure()
-        plot0 = plt.imshow(data, interpolation='nearest',norm=None)
-        
-        fig.colorbar(plot0)
+    fig = plt.figure()
+    plt.plot(time_points[1:], middle_point_speed_x, 'cyan',
+             label='Speed in x dir of point %i/%i' % (middle_point,
+                                                      points))
+    plt.plot(time_points[1:], middle_point_speed_y, 'red',
+             label='Speed in y dir of point %i/%i' % (middle_point,
+                                                      points))
+    plt.plot(time_points[1:], ave_point_speed_x, 'blue',
+             label='Speed in x of average of %i points' % points)
+    plt.plot(time_points[1:], ave_point_speed_y, 'green',
+             label='Speed in y of average of %i points' % points)
+    plt.legend()
+
+    if save_figure2_to:
+        plt.savefig(save_figure2_to,bbox_inches='tight')
+
+    fig = plt.figure()
+    plot0 = plt.imshow(data, interpolation='nearest',norm=None)
+
+    fig.colorbar(plot0)
+
+    if save_figure3_to:
+        plt.savefig(save_figure3_to,bbox_inches='tight')
+            
+    if plot:
         plt.show()
         
 
@@ -225,7 +241,7 @@ def validate(wcon_file):
     wcon_schema = "wcon_schema.json"
     
     if not os.path.isfile("wcon_schema.json"):
-        print("Cannot validate file: %s!! WCON schema %s not found!!"%(wcon_file, wcon_schema))
+        print_("Cannot validate file: %s!! WCON schema %s not found!!"%(wcon_file, wcon_schema))
         return
 
     # The WCON schema
@@ -242,7 +258,7 @@ def validate(wcon_file):
     # Validate the raw file against the WCON schema
     jsonschema.validate(w, schema)
     
-    print("File %s is valid WCON!!"%wcon_file)
+    print_("File %s is valid WCON!!"%wcon_file)
 
 
 if __name__ == '__main__':
