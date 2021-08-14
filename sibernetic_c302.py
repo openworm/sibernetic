36148b691f7dd375c4c9bce906e24f041406c2f3
Mark Watts <watts.mark2015@gmail.com>
2021-08-14 16:50:30 -0500

    Adding XAUTHORITY env var to sibernetic command

--- sibernetic_c302.py
+++ sibernetic_c302.py
@@ -328,6 +328,7 @@ def run(a=None,**kwargs):
     command = './Release/Sibernetic %s -f %s -no_g -l_to lpath=%s timelimit=%s timestep=%s logstep=%s device=%s'%('' if a.noc302 else '-c302', a.configuration, sim_dir, a.duration/1000.0, a.dt/1000, a.logstep, a.device)
 
     env = {"DISPLAY": os.environ.get('DISPLAY'),
+            "XAUTHORITY": os.environ.get('XAUTHORITY'),
             "PYTHONPATH": ".:%s:%s" % (os.environ.get('PYTHONPATH', '.'), os.path.abspath(sim_dir))}
 
     sim_start = time.time()
@@ -489,5 +490,3 @@ def run(a=None,**kwargs):
 
 if __name__ == '__main__':
     main()
-
-
