90ab231fbbecc59c17b022aa143baa4e7f1626f5
Austin Klein <austinkl@usc.edu>
2023-09-03 13:01:33 -0700

    Remove endl

--- src/owHelper.cpp
+++ src/owHelper.cpp
@@ -360,7 +360,7 @@ void owHelper::loadConfiguration(float *position_cpp, float *velocity_cpp,
   } else
     throw std::runtime_error("Could not open file configuration file");
   configFile.close();
-  std::cout << "Configuration has been loaded" << std::endl;
+  std::cout << "Configuration has been loaded change\n";
   std::cout << "in load end of load\n";
 }
 /** Load configuration from simulation to files
