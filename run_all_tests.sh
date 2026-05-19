f3938809ac1368d6291e125439ab2aa0c1907542
Padraig Gleeson <p.gleeson@gmail.com>
2026-05-19 19:59:22 +0100

    Better test

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,6 +1,10 @@
-#This script will run a number of tests using sibernetic_c302.py & check the 
-# files produced
+#!/bin/bash
 set -ex
+
+#This script will run a number of tests using sibernetic_c302.py & check the files produced
+
+# Cube only
+python3 sibernetic_c302.py -test -noc302 -duration 30 -dt 0.005 -configuration demo1 -simName test_cube -logstep 10 -q
 
 # No c302
 python3 sibernetic_c302.py -test -noc302 -duration 0.1 -simName test_noc302 
