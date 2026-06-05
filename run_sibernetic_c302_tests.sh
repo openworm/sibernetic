2e476c9634ab5b72780498881fd60ab39298d4be
Padraig Gleeson <p.gleeson@gmail.com>
2026-06-05 11:08:47 +0100

    Run omv tests sooner to highlight issues

--- run_sibernetic_c302_tests.sh
+++ run_sibernetic_c302_tests.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -ex
 
-#This script will run a number of tests using sibernetic_c302.py & check the files produced
+# This script will run a number of tests using sibernetic_c302.py & check the files produced
+
+TEST_ALL=false
+[[ $# -eq 1 && $1 == '-all' ]] && TEST_ALL=true
+
 
 # Cube only
 python3 sibernetic_c302.py -test -noc302 -duration 30 -dt 0.02 -configuration demo1 -simName test_cube -logstep 10 -q
+
+if $TEST_ALL; then omv test tests/.test.cube.omt -V; fi
 
 # No c302 but with the worm body 
 python3 sibernetic_c302.py -test -noc302 -duration 0.1 -simName test_noc302 
@@ -24,7 +30,7 @@ python3 sibernetic_c302.py -test -duration 20 -c302params C0 -reference TargetMu
 # c302 + C2 FW 
 python sibernetic_c302.py -test -duration 15 -c302params C2 -reference FW -configuration worm_crawl_half_resolution -logstep 100
 
-if [[ ($# -eq 1) && ($1 == '-all') ]]; then
+if $TEST_ALL; then
 
     # Run a simulation with the FW (forward locomotion) c302 configuration with C2 (cond based) cells
     python sibernetic_c302.py -test -duration 150 -dt 0.005 -dtNrn 0.05 -logstep 100 -device=CPU -configuration worm_crawl_half_resolution -reference FW -c302params C2 -datareader UpdatedSpreadsheetDataReader2 -simName test_C2_FW -q
