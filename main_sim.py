3ef563de7ccc4a3678d6a827973ec88ac511ace0
Padraig Gleeson <p.gleeson@gmail.com>
2026-09-03 15:56:12 +0100

    vr also

--- main_sim.py
+++ main_sim.py
@@ -278,20 +278,29 @@ class C302NRNSimulation:
             )
             values.append(scaled_val)
             vars_read.append(var)
-        for i in range(24): # VR muscles
-            var = "a_MVR%s" % (i + 1 if i > 8 else ("0%i" % (i + 1)))
-            if i == 23:
-                var = "a_MVR23"
+        for i in range(24):
+            DV = "V"
+            RL = "R"
+            index = i + 1
+            zero = "0" if index <= 9 else ""
+
+            var = self.var_template.format(DV, RL, zero, index)
+
             try:
-                val = getattr(self.h, var)[0].soma.cai
+                h_obj = getattr(self.h, var)[0 if "{3}" in self.var_template else i]
+                val = (
+                    getattr(h_obj.soma, self.var_name)
+                    if hasattr(h_obj, "soma")
+                    else getattr(h_obj, self.var_name)
+                )
             except AttributeError as e:
-                print(
+                raise Exception(
                     "Problem passing neuronal output of %s to muscle in Sibernetic: %s"
                     % (var, e)
                 )
-                val = 0
-                continue
-            scaled_val = self._scale(val)
+            scaled_val = self._scale(
+                val, print_it=self.print_it, scale_it=self.scale_it
+            )
             values.append(scaled_val)
             vars_read.append(var)
         for i in range(24): # VL muscles
